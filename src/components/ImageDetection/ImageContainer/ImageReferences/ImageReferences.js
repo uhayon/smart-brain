10507b0f0f830881a4c97151a0ac5d74771061dc
Uriel Hayon <uri.a77aque@gmail.com>
2019-02-26 11:15:04 -0300

    fix on no matches message

--- src/components/ImageDetection/ImageContainer/ImageReferences/ImageReferences.js
+++ src/components/ImageDetection/ImageContainer/ImageReferences/ImageReferences.js
@@ -33,33 +33,39 @@ const ImageReferences = ({ isSearching, references, lastSearchedModel, selectedM
           (
             lastSearchedModel === selectedModel && references.length === 0 ?
             <p className='pa2 ttc'>There's no elements that match what you are looking for in the provided picture</p> :
-            references.map(({ description, key }) =>
-              <li 
-                style={
-                  selectedModel === 'COLOR_MODEL' ?
-                  {
-                    backgroundColor: key
-                  } :
-                  {}
-                }
-                className={`${referenceElement} pointer pa2 ttc`}
-                onMouseOver={() => highlightBox(key)}
-                onMouseOut={() => unhighlightBox(key)}
-                key={description}
-                data-reference={key} >
-                <span
-                  style={
-                    selectedModel === 'COLOR_MODEL' ?
-                    {
-                      backgroundColor: 'rgba(0, 0, 0, .75)',
-                    color: '#fff'
-                    } :
-                    {}
-                  }
-                >
-                  {description}
-                </span>
-              </li>
+            (
+              lastSearchedModel === selectedModel && references.length > 0 ?
+                (
+                  references.map(({ description, key }) =>
+                  <li 
+                    style={
+                      selectedModel === 'COLOR_MODEL' ?
+                      {
+                        backgroundColor: key
+                      } :
+                      {}
+                    }
+                    className={`${referenceElement} pointer pa2 ttc`}
+                    onMouseOver={() => highlightBox(key)}
+                    onMouseOut={() => unhighlightBox(key)}
+                    key={description}
+                    data-reference={key} >
+                    <span
+                      style={
+                        selectedModel === 'COLOR_MODEL' ?
+                        {
+                          backgroundColor: 'rgba(0, 0, 0, .75)',
+                        color: '#fff'
+                        } :
+                        {}
+                      }
+                    >
+                      {description}
+                    </span>
+                  </li>
+                )
+              ) :
+              null
             )
           )
         }
